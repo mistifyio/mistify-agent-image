f76e897b7d849ffb7a728fe7a4dfb7318a3da4bd
Brian Akins <bakins@omniti.com>
2014-10-03 11:13:28 -0400

    add test

---
+++ test.sh
@@ -0,0 +1,30 @@
+#/usr/bin/env bash
+set -e
+set -o pipefail
+set -x
+
+ADDRESS=${1:-127.0.0.1:16000}
+
+request (){
+    METHOD=$1
+    shift
+    ID=$RANDOM
+    PARAMS="$@"
+
+    DATA=$(printf '{ "id": %d, "method": "ImageStore.%s", "params": [%s] }' $ID $METHOD "$PARAMS")
+    
+    curl --fail -sv -X POST -H 'Content-Type: application/json' http://$ADDRESS/_mistify_RPC_ -d "$DATA" | jq .
+}
+
+request ListVolumes
+
+request CreateVolume '{"id": "guests/foo", "size": 8 }'
+
+request ListVolumes
+
+request DeleteDataset '{"id": "guests/foo" }'
+
+request ListVolumes
+
+
+
