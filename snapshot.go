ea99dace268f21342cbc88be899f55fb39523435
Brian Bickerton <bbickerton@omniti.com>
2014-10-24 19:45:16 +0000

    MIST-150 Add docs of the methods and their request parameters

--- snapshot.go
+++ snapshot.go
@@ -15,6 +15,13 @@ func snapshotFromDataset(ds *zfs.Dataset) *rpc.Snapshot {
 	}
 }
 
+/*
+CreateSnapshot creates a snapshot of a zfs dataset.
+    Request params:
+    id        string : Req : Id of the zfs dataset to snapshot
+    dest      string : Req : Name of the snapshot
+    recursive bool   :     : Recursively create snapshots of descendents
+*/
 func (store *ImageStore) CreateSnapshot(r *http.Request, request *rpc.SnapshotRequest, response *rpc.SnapshotResponse) error {
 	if request.Id == "" {
 		return errors.New("need an id")
@@ -63,6 +70,12 @@ func (store *ImageStore) getSnapshot(id string) (*zfs.Dataset, error) {
 	return ds, nil
 }
 
+/*
+DeleteSnapshot deletes a snapshot.
+    Request params:
+    id        string : Req : Full name of the snapshot
+    recursive bool   :     : Recursively delete descendent snapshots
+*/
 func (store *ImageStore) DeleteSnapshot(r *http.Request, request *rpc.SnapshotRequest, response *rpc.SnapshotResponse) error {
 	if request.Id == "" {
 		return errors.New("need an id")
@@ -85,6 +98,11 @@ func (store *ImageStore) DeleteSnapshot(r *http.Request, request *rpc.SnapshotRe
 	return nil
 }
 
+/*
+GetSnapshot retrieves information about a snapshot.
+    Request params:
+    id        string : Req : Full name of the snapshot
+*/
 func (store *ImageStore) GetSnapshot(r *http.Request, request *rpc.SnapshotRequest, response *rpc.SnapshotResponse) error {
 	if request.Id == "" {
 		return errors.New("need an id")
@@ -103,6 +121,11 @@ func (store *ImageStore) GetSnapshot(r *http.Request, request *rpc.SnapshotReque
 	return nil
 }
 
+/*
+ListSnapshots retrieves a list of all snapshots for a dataset.
+    Request params:
+    id        string :     : Dataset to list snapshots for
+*/
 func (store *ImageStore) ListSnapshots(r *http.Request, request *rpc.SnapshotRequest, response *rpc.SnapshotResponse) error {
 	filter := store.config.Zpool
 	if request.Id != "" {
@@ -115,7 +138,7 @@ func (store *ImageStore) ListSnapshots(r *http.Request, request *rpc.SnapshotReq
 	}
 
 	snapshots := make([]*rpc.Snapshot, len(datasets))
-	for i, _ := range datasets {
+	for i := range datasets {
 		snapshots[i] = snapshotFromDataset(datasets[i])
 	}
 
