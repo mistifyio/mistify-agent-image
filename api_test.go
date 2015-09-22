567ae905ca5e35434f99fc2b81bfffe6844ac810
Brian Bickerton <brian.bickerton@gmail.com>
2015-09-23 02:17:56 +1000

    Move the ImageService to the general APITestSuite setup

--- api_test.go
+++ api_test.go
@@ -1,9 +1,15 @@
 package imagestore_test
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"math"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
+	"path/filepath"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -18,20 +24,37 @@ import (
 
 type APITestSuite struct {
 	suite.Suite
-	ID          string
-	ZpoolDir    string
-	Zpool       *zfs.Zpool
-	Port        int
-	StoreConfig imagestore.Config
-	Store       *imagestore.ImageStore
-	Server      *graceful.Server
-	Client      *rpc.Client
+	ID           string
+	ZpoolDir     string
+	Zpool        *zfs.Zpool
+	Port         int
+	StoreConfig  imagestore.Config
+	Store        *imagestore.ImageStore
+	Server       *graceful.Server
+	Client       *rpc.Client
+	ImageService *httptest.Server
+	ImageID      string
+	ImageData    []byte
 }
 
 func (s *APITestSuite) SetupSuite() {
 	log.SetLevel(log.FatalLevel)
 	s.Port = 54321
 	s.Client, _ = rpc.NewClient(uint(s.Port), "")
+
+	s.ImageService = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != fmt.Sprintf("/images/%s/download", s.ImageID) {
+			http.NotFound(w, r)
+			return
+		}
+		if _, err := w.Write(s.ImageData); err != nil {
+			log.WithField("error", err).Error("Failed to write mock image data to response")
+		}
+	}))
+
+	imageURL, _ := url.Parse(s.ImageService.URL)
+
+	s.StoreConfig.ImageServer = imageURL.Host
 }
 
 func (s *APITestSuite) SetupTest() {
@@ -59,6 +82,21 @@ func (s *APITestSuite) SetupTest() {
 	s.Zpool, err = zfs.CreateZpool(s.ID, nil, zpoolFileNames...)
 	require.NoError(err, "create zpool")
 
+	if s.ImageID == "" {
+		// Set up the image to be served from the test "image service" by
+		// creating a volume, exporting a snapshot, and cleaning up. Only needs
+		// to be done once, but can use an existing zpool if done in test setup.
+		s.ImageID = uuid.New()
+		volume, err := zfs.CreateVolume(filepath.Join(s.ID, s.ImageID), uint64(1*1024*1024), defaultZFSOptions)
+		s.Require().NoError(err)
+		snapshot, err := volume.Snapshot("test", false)
+		s.Require().NoError(err)
+		buff := new(bytes.Buffer)
+		s.Require().NoError(snapshot.SendSnapshot(buff))
+		s.ImageData = buff.Bytes()
+		s.Require().NoError(volume.Destroy(zfs.DestroyRecursive))
+	}
+
 	// Run the ImageStore
 	s.Store, err = imagestore.Create(s.StoreConfig)
 	require.NoError(err)
@@ -74,6 +112,15 @@ func (s *APITestSuite) TearDownTest() {
 	logx.LogReturnedErr(s.Zpool.Destroy, nil, "unable to destroy zpool "+s.ID)
 	logx.LogReturnedErr(func() error { return os.RemoveAll(s.ZpoolDir) },
 		nil, "unable to remove dir "+s.ZpoolDir)
+}
+
+func (s *APITestSuite) fetchImage() *rpc.Image {
+	response := &rpc.ImageResponse{}
+	request := &rpc.ImageRequest{
+		ID: s.ImageID,
+	}
+	_ = s.Client.Do("ImageStore.RequestImage", request, response)
+	return response.Images[0]
 }
 
 func init() {
--- image_test.go
+++ image_test.go
@@ -1,16 +1,9 @@
 package imagestore_test
 
 import (
-	"bytes"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
 	"path/filepath"
 	"testing"
 
-	log "github.com/Sirupsen/logrus"
-	"github.com/mistifyio/go-zfs"
 	"github.com/mistifyio/mistify-agent/rpc"
 	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/suite"
@@ -24,59 +17,10 @@ type imageTestCase struct {
 
 type ImageTestSuite struct {
 	APITestSuite
-	ImageService *httptest.Server
-	ImageID      string
-	ImageData    []byte
-}
-
-func (s *ImageTestSuite) SetupSuite() {
-	s.APITestSuite.SetupSuite()
-
-	s.ImageService = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != fmt.Sprintf("/images/%s/download", s.ImageID) {
-			http.NotFound(w, r)
-			return
-		}
-		if _, err := w.Write(s.ImageData); err != nil {
-			log.WithField("error", err).Error("Failed to write mock image data to response")
-		}
-	}))
-
-	imageURL, _ := url.Parse(s.ImageService.URL)
-
-	s.StoreConfig.ImageServer = imageURL.Host
-}
-
-func (s *ImageTestSuite) SetupTest() {
-	s.APITestSuite.SetupTest()
-
-	if s.ImageID == "" {
-		// Set up the image to be served from the test "image service" by
-		// creating a volume, exporting a snapshot, and cleaning up. Only needs
-		// to be done once, but can use an existing zpool if done in test setup.
-		s.ImageID = uuid.New()
-		volume, err := zfs.CreateVolume(filepath.Join(s.ID, s.ImageID), uint64(1*1024*1024), defaultZFSOptions)
-		s.Require().NoError(err)
-		snapshot, err := volume.Snapshot("test", false)
-		s.Require().NoError(err)
-		buff := new(bytes.Buffer)
-		s.Require().NoError(snapshot.SendSnapshot(buff))
-		s.ImageData = buff.Bytes()
-		s.Require().NoError(volume.Destroy(zfs.DestroyRecursive))
-	}
 }
 
 func TestImageTestSuite(t *testing.T) {
 	suite.Run(t, new(ImageTestSuite))
-}
-
-func (s *ImageTestSuite) fetchImage() *rpc.Image {
-	response := &rpc.ImageResponse{}
-	request := &rpc.ImageRequest{
-		ID: s.ImageID,
-	}
-	_ = s.Client.Do("ImageStore.RequestImage", request, response)
-	return response.Images[0]
 }
 
 func (s *ImageTestSuite) runTestCases(method string, tests []*imageTestCase) {
